1e49aa1606bb804b61fb3753e4988c6b48ff2d8d
subnare <shrutikaubnare@gmail.com>
2026-03-10 14:02:35 -0400

    Add project files.

---
+++ HealthHabitApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthHabitApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ HealthHabitApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthHabitApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
